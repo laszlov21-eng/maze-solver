62877c02c5a6411ab00d270d06f286bad64a1b22
Mercucio <laszlo.v21@gmail.com>
2026-01-19 17:40:33 +0100

    basic cell to cell lines

--- cell.py
+++ cell.py
@@ -14,6 +14,8 @@ class Cell:
         self.__win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self.__win is None:
+            return
         self.__x1 = x1
         self.__x2 = x2
         self.__y1 = y1
@@ -31,5 +33,14 @@ class Cell:
             line = Line(Point(x1, y2), Point(x2, y2))
             self.__win.draw_line(line)
     
-    def draw_move(self,to_cell,undo = False):
-        pass
+    def draw_move(self, to_cell, undo = False):
+        if self.__win is None:
+            return
+        sp_x = (self.__x1 + self.__x2) // 2
+        sp_y = (self.__y1 + self.__y2) // 2
+        ep_x = (to_cell.__x1 + to_cell.__x2) // 2
+        ep_y = (to_cell.__y1 + to_cell.__y2) // 2
+        path = Line(Point(sp_x,sp_y),Point(ep_x,ep_y))
+        fill_color = "gray" if undo else "red"
+        self.__win.draw_line(path,fill_color)
+        
